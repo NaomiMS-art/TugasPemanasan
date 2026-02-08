f92c3845bcfecf0f26d71bc3bdaf9850bddeb242
NaomiMS-seni <mtrsmmr@gmail.com>
2026-02-08 19:55:34 +0700

    tugas

---
+++ script.js
@@ -0,0 +1,33 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+const cursor = document.querySelector(".cursor");
+  const follower = document.querySelector(".cursor-follower");
+
+  let mouseX = 0, mouseY = 0;
+  let followerX = 0, followerY = 0;
+
+  document.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    cursor.style.left = mouseX + "px";
+    cursor.style.top = mouseY + "px";
+  });
+
+  function animate() {
+    followerX += (mouseX - followerX) * 0.15;
+    followerY += (mouseY - followerY) * 0.15;
+
+    follower.style.left = followerX + "px";
+    follower.style.top = followerY + "px";
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+
+  
